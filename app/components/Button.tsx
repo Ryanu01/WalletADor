d4d3060c01eddbcd9f2622ff32efd63266b579ff
Ryanu01 <ryangomes08090109@gmail.com>
2025-10-29 00:39:33 +0530

    improve button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -50,7 +50,7 @@ export const TabButton = ({ active, children, onClick }: {
 }) => {
     return (
         
-            <button type="button" className={`mt-5 max-w-xs text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ${active ? "bg-blue-500" : "bg-blue-300"}`} onClick={onClick}>{children}</button>
+            <button type="button" className={`mt-5 w-full text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ${active ? "bg-blue-500" : "bg-blue-300"}`} onClick={onClick}>{children}</button>
         
     )
 }
--- app/components/ProfileCard.tsx
+++ app/components/ProfileCard.tsx
@@ -11,7 +11,7 @@ import {  useTokens } from "../api/hooks/useToken";
 import { TokenList, TokenRowSkeleton } from "./TokenList";
 
 type Tab = "tokens" | "send" | "add_funds" | "swap" | "withdraw"
-const tabs: Tab[] = ["tokens", "send", "add_funds", "swap", "withdraw"];
+const tabs: {id: Tab, name: string}[] = [{ id: "tokens", name:  "Token"}, {id: "send", name: "Send"}, {id: "add_funds", name: "Add Funds"}, {id: "swap", name: "Swap"}, {id: "withdraw", name: "Withdraw"}];
 
 export const ProfileCard = ({publicKey}: {
     publicKey: string
@@ -33,9 +33,11 @@ export const ProfileCard = ({publicKey}: {
                     image={session.data?.user?.image ?? ""}
                     name={session.data?.user?.name ?? ""}
                 />
-                {tabs.map(tab => <TabButton active={tab === selectedtab} onClick={() => {
-                    setSelectedTab(tab)
-                }}>{tab}</TabButton>)}
+                <div className="w-full flex">
+                    {tabs.map(tab => <TabButton active={tab.name === selectedtab} onClick={() => {
+                        setSelectedTab(tab.name)
+                    }}>{tab.name.toLocaleUpperCase()}</TabButton>)}
+                </div>
                 <div className={`${selectedtab === "tokens" ? "visible"  : "hidden"}`}><Assets publicKey={publicKey} />
                 </div>
             </div>
